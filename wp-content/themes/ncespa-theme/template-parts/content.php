858d5e96257d2c34637269e9bfb5ceb115b7db32
Caroline Horn <caroline@curiouschaos.com>
2016-09-18 22:42:59 -0400

    started featured posts

--- wp-content/themes/ncespa-theme/template-parts/content.php
+++ wp-content/themes/ncespa-theme/template-parts/content.php
@@ -10,25 +10,32 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-			<span class="sticky-post"><?php _e( 'Featured', 'ncespa' ); ?></span>
-		<?php endif; ?>
+	<?php ncespa_post_thumbnail(); ?>
 
+	<header class="entry-header">
+	  <span class="date"><?php the_date() ?></span>
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php ncespa_excerpt(); ?>
-
-	<?php ncespa_post_thumbnail(); ?>
-
 	<div class="entry-content">
+  	<?php ncespa_excerpt(); ?>
+
 		<?php
+		if ( is_single() ) :
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'ncespa' ),
-				get_the_title()
+				__( 'Continue reading %s', 'ncespa' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
 			) );
+
+			else :
+
+  			/* translators: %s: Name of current post */
+  			the_excerpt( sprintf(
+  				__( 'Continue reading %s', 'twentysixteen' ),
+  				the_title( '<span class="screen-reader-text">', '</span>', false )
+  			) );
+			endif;
 
 			wp_link_pages( array(
 				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'ncespa' ) . '</span>',
@@ -41,18 +48,17 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php ncespa_entry_meta(); ?>
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'ncespa' ),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
+	<!--
+<?php
+		edit_post_link(
+			sprintf(
+				/* translators: %s: Name of current post */
+				__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'ncespa' ),
+				get_the_title()
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
+	?>
+-->
 </article><!-- #post-## -->
